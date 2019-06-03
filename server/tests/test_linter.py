c730bf08e00fb5f3222eaed14552fa31e6a4ef22
cwperks <craig5008@gmail.com>
2019-06-03 15:51:22 -0400

    Add dummy tests

---
+++ server/tests/test_linter.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+
+import sys, os
+path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, path + '/../')
+
+import pytest
+
+from app import app
+
+@pytest.fixture
+def client():
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+
+def test_empty_db(client):
+    """Start with a blank database."""
+
+    # rv = client.get('/')
+    assert 1 + 1 == 2
